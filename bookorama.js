bede2e5e91c8ed62deb464b7387b66043c9c9228
mstpierre73 <naranjaqc@hotmail.com>
2019-03-27 16:43:54 -0400

    final project

--- bookorama.js
+++ bookorama.js
@@ -1,41 +1,84 @@
-/*Project inspired by The Net Ninja-->
+/*Project inspired by The Net Ninja*/
 
-/*Delete books*/
-const list = document.querySelector("#book-list ul");
-list.addEventListener("click", function(e){
-	if(e.target.className === "delete"){
-		const li = e.target.parentElement;
-		list.removeChild(li);
-	}
-});
+document.addEventListener('DOMContentLoaded', function(){
 
-/*Adding books*/
-const addForm = document.forms["add-book"];
-addForm.addEventListener("submit", function(e){
-	e.preventDefault();
-	const value = addForm.querySelector("input[type='text']").value;
-	
-	/*create elements*/
-	const newli = document.createElement("li");
-	const bookName = document.createElement("span");
-	const deletebtn = document.createElement("span");
+  const list = document.querySelector('#book-list ul');
+  const forms = document.forms;
 
-	/*add content */
-	deletebtn.textContent = "delete";
-	bookName.textContent = value;
+  // delete books
+  list.addEventListener('click', (e) => {
+    if(e.target.className == 'delete'){
+      const li = e.target.parentElement;
+      li.parentNode.removeChild(li);
+    }
+  });
 
-	/*append to the DOM*/
-	newli.appendChild(bookName);
-	newli.appendChild(deletebtn);
-	list.appendChild(newli);
+  // add books
+  const addForm = forms['add-book'];
+  addForm.addEventListener('submit', function(e){
+    e.preventDefault();
 
-	
-	/*Style the new element*/
-	const lastLi = document.querySelector("li:last-child");
-	bookName.classList.add("name");
-	deletebtn.classList.add("delete");
-});
+    // create elements
+    const value = addForm.querySelector('input[type="text"]').value;
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
 
+    // add text content
+    bookName.textContent = value;
+    deleteBtn.textContent = 'delete';
 
+    // add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    // append to DOM
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+  });
+
+  // hide books
+  const hideBox = document.querySelector('#hide');
+  hideBox.addEventListener('change', function(e){
+    if(hideBox.checked){
+      list.style.display = "none";
+    } else {
+      list.style.display = "initial";
+    }
+  });
+
+  // filter books
+  const searchBar = forms['search-books'].querySelector('input');
+  searchBar.addEventListener('keyup', (e) => {
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach((book) => {
+      const title = book.firstElementChild.textContent;
+      if(title.toLowerCase().indexOf(e.target.value) != -1){
+        book.style.display = 'block';
+      } else {
+        book.style.display = 'none';
+      }
+    });
+  });
+
+  // tabbed content
+  const tabs = document.querySelector('.tabs');
+  const panels = document.querySelectorAll('.panel');
+  tabs.addEventListener('click', (e) => {
+    if(e.target.tagName == 'LI'){
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      Array.from(panels).forEach((panel) => {
+        if(panel == targetPanel){
+          panel.classList.add('active');
+        }else{
+          panel.classList.remove('active');
+        }
+      });
+    }
+  });
+
+}); //end of callback function
 
 
